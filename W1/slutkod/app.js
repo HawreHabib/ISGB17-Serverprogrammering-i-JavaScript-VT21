9f3fd967a67bb83be58058f577633f4f6d14efa7
Peter Bellström <peter.bellstrom@kau.se>
2021-04-29 11:04:39 +0200

    Uppdaterat W1

--- W1/slutkod/app.js
+++ W1/slutkod/app.js
@@ -1,3 +1,19 @@
 'use strict';
 
 //app.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const jsDOM = require('jsdom');
+const fs = require('fs');
+
+let app = express();
+
+//Två middleware: ett för express och ett för body-parser
+app.use('/public', express.static(__dirname + '/static') );
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.listen(81, function() {
+    console.log('Server is running on port 81!');
+});
+
